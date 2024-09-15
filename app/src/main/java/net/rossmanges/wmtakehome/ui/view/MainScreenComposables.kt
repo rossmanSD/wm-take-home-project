368b8cdfa06865b27e80de4421f7cab006d5bccb
Ross Manges <2281039+rossmanSD@users.noreply.github.com>
2024-09-15 12:24:14 -0700

    Moving all Composables from MainActivity to their own file.

---
+++ app/src/main/java/net/rossmanges/wmtakehome/ui/view/MainScreenComposables.kt
@@ -0,0 +1,44 @@
+package net.rossmanges.wmtakehome.ui.view
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.rossmanges.wmtakehome.data.Country
+
+/**
+ * The Composable function that describes the main scaffold and layout for this app.
+ */
+@Composable
+fun MainScreen(filteredCountries: List<Country>, onSearchTextChanged: (String) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBarWithSearch(
+                onSearchTextChanged = { onSearchTextChanged(it) }
+            )
+        },
+        content = { padding ->
+            Surface(
+                modifier = Modifier
+                    .padding(
+                        top = padding.calculateTopPadding(),
+                        bottom = padding.calculateBottomPadding(),
+                        start = 2.dp,
+                        end = 7.dp
+                    )
+                    .fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                if (filteredCountries.isEmpty()) {
+                    NoDataMessage()
+                } else {
+                    CountryList(countries = filteredCountries)
+                }
+            }
+        }
+    )
+}
